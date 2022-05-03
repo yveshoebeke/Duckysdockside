8d2a6707f58d56b3d57c7c4f0488da46cb2354af
yveshoebeke <yves.hoebeke@gmail.com>
2022-05-03 15:18:07 -0400

    Added image carousel

---
+++ static/js/carousel.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $("#carousel1").show()
+    $("#carousel2").hide()
+    $("#carousel3").hide()
+    var carousel = 1;
+    setInterval(function() {
+    id = "#carousel" + carousel
+        $(id).fadeTo("slow", 0.05,function(){
+            $(id).hide()
+            carousel += 1
+            if (carousel > 3) {
+                carousel = 1
+            }
+            id = "#carousel" + carousel
+            $(id).fadeIn("slow")
+        })
+   }, 20000);
+})
